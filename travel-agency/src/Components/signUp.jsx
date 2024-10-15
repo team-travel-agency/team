6dc7ac0642f1dff2136c70394cdd9c34b173026f
Rahma-salhy <rahmasalhi2112@gmail.com>
2024-10-15 16:48:43 +0100

    signup front

--- travel-agency/src/Components/signUp.jsx
+++ travel-agency/src/Components/signUp.jsx
@@ -1,47 +1,58 @@
-import React , {useState} from "react"
-const Login=()=>{
-    const [userName , setUserName]= useState("")
-    const [email , setEmail]= useState("")
-    const [password , setPassword]= useState("")
-    const [confirmePassword , setConfirmePassword]= useState("")
+import React, { useState } from "react";
+import axios from "axios";
 
-    
-const handleLogin =(e)=>{
-e.preventDefault()
-}
-return (
+const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const userData = {
+      userName,
+      email,
+      password,
+      
+    };
+
+    try {
+      const response = await axios.post("/api/signup", userData);
+      console.log("Response:", response.data);
+     
+    } catch (error) {
+      console.error("Error during sign up:", error);
+      
+    }
+  };
+
+  return (
     <div>
-    <h2>Sign Up</h2>
-    <form onSubmit={handleLogin}>
-    <input 
-    type="text"
-    placeholder="enter your name"
-    value={userName}
-    onChange={(e)=>{setUserName(e.target.value)}}
-    />
-<input 
-    type="text"
-    placeholder="enter your email"
-    value={email}
-    onChange={(e)=>{setEmail(e.target.value)}}
-    />
- 
- <input 
-    type="text"
-    placeholder="enter your password"
-    value={password}
-    onChange={(e)=>{setPassword(e.target.value)}}
-    />
-
-<input 
-    type="text"
-    placeholder="confimre your password"
-    value={confirmePassword}
-    onChange={(e)=>{setConfirmePassword(e.target.value)}}
-    />
-<button type="submit"> Sign Up </button>
-    </form>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={userName}
+          onChange={(e) => { setUserName(e.target.value); }}
+        />
+        <input
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => { setEmail(e.target.value); }}
+        />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => { setPassword(e.target.value); }}
+        />
+       
+        <button type="submit">Sign Up</button>
+      </form>
     </div>
-)
-}
-export default Login 
+  );
+};
+
+export default Login;
